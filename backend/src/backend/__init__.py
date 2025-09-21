9d0e7422bde6af1fad22bd945c54dfe13b4d869f
eliseydudin <dudinelisey630@gmail.com>
2025-09-21 20:23:51 +0300

    kolyan fix

--- backend/src/backend/__init__.py
+++ backend/src/backend/__init__.py
@@ -1,2 +1,6 @@
+from .database import check
+
+
 def main() -> None:
     print("Hello from backend!")
+    check()
--- backend/src/backend/database.py
+++ backend/src/backend/database.py
@@ -1,18 +1,21 @@
 import psycopg2
-from config import host, user, password, db_name, port
+from .config import host, user, password, db_name
 
-try:
-    connection = psycopg2.connect(
-        host=host,
-        user=user,
-        password=password,
-        database = db_name,
-        options="-c client_encoding=UTF8"
-    )
-    with connection.cursor() as cursor:
-        cursor.execute("SELECT version()")
-        print(cursor.fetchone())
-except Exception as ex:
-    print(f"[INFO] Error while working with PostgreSQL: {ex}")
-finally:
-    pass
+
+def check():
+    try:
+        connection = psycopg2.connect(
+            host=host,
+            user=user,
+            password=password,
+            database=db_name,
+            options="-c client_encoding=UTF8",
+        )
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT version()")
+            print(cursor.fetchone())
+
+    except Exception as ex:
+        print(f"[INFO] Error while working with PostgreSQL: {ex}")
+    finally:
+        pass
